ab35cac2e94663a9a8f569e41597141f842c3e7f
Danyl Fernandes <androidan1304@gmail.com>
2020-03-18 05:43:42 +0530

    Implemented Phone Call

--- app/src/main/java/com/realllydan/yakira/fragments/HomeFragment.java
+++ app/src/main/java/com/realllydan/yakira/fragments/HomeFragment.java
@@ -1,15 +1,14 @@
 package com.realllydan.yakira.fragments;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.appcompat.widget.Toolbar;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -17,12 +16,22 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.realllydan.yakira.Constants;
@@ -35,10 +44,14 @@ import com.realllydan.yakira.data.models.Member;
 
 import java.util.ArrayList;
 
+import javax.annotation.Nullable;
+
 public class HomeFragment extends Fragment implements
         MemberListAdapter.OnMemberClickListener,
         MemberListAdapter.OnCallClickListener {
 
+    private static final int CALL_PERMISSION_REQUEST_CODE = 1;
+    private static final int CALL_INTENT_REQUEST_CODE = 2;
     private static final String TAG = "HomeFragment";
 
     private FloatingActionButton fabAddMember;
@@ -49,6 +62,7 @@ public class HomeFragment extends Fragment implements
     private HomeFragmentListener homeFragmentListener;
     private IMainActivity iMainActivity;
     private ArrayList<Member> memberArrayList = new ArrayList<>();
+    private MemberListAdapter memberListAdapter;
 
     public HomeFragment() {
 
@@ -56,14 +70,40 @@ public class HomeFragment extends Fragment implements
 
     @Override
     public void onMemberClick(int pos) {
-        String clickedMemberName = memberArrayList.get(pos).getName();
-        Snackbar.make(view, clickedMemberName, Snackbar.LENGTH_SHORT).show();
+        //TODO: View and Edit Member details
+
+        String underDevelopment = "This feature is under development ;)";
+        Snackbar.make(view, underDevelopment, Snackbar.LENGTH_LONG).show();
     }
 
     @Override
     public void onCallClick(int pos) {
-        String clickedMemberName = memberArrayList.get(pos).getName();
-        Snackbar.make(view, "Calling " + clickedMemberName + "...", Snackbar.LENGTH_SHORT).show();
+        String memberContactNumber = memberArrayList.get(pos).getContact();
+
+        if (memberContactNumber != null) {
+
+            if (isPhoneCallPermissionGranted()) {
+                makePhoneCallToMember(memberContactNumber);
+            } else {
+                requestPhoneCallPermission();
+            }
+
+        } else {
+            Snackbar.make(view, R.string.contact_null_error_message, Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CALL_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Snackbar.make(view, R.string.permission_granted_acknowledgement, Snackbar.LENGTH_LONG).show();
+            } else {
+                Snackbar.make(view, R.string.permission_denied_acknowledgement, Snackbar.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
@@ -75,7 +115,7 @@ public class HomeFragment extends Fragment implements
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view =  inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
         mAuth = FirebaseAuth.getInstance();
 
         iMainActivity.setToolbarTitle(R.string.home_fragment_title);
@@ -132,7 +172,7 @@ public class HomeFragment extends Fragment implements
         setupRecyclerViewData();
 
         RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
-        MemberListAdapter memberListAdapter = new MemberListAdapter(memberArrayList, this, this);
+        memberListAdapter = new MemberListAdapter(memberArrayList, this, this);
         recyclerView.setAdapter(memberListAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
@@ -140,29 +180,24 @@ public class HomeFragment extends Fragment implements
     private void setupRecyclerViewData() {
         memberArrayList.clear();
 
-        /*
-        Dummy Data
-
-        memberArrayList.add(new Member("Danyl Fernandes", "Team"));
-        memberArrayList.add(new Member("Joel Fernandes", "Team"));
-        memberArrayList.add(new Member("Carol Fernandes", "General"));
-        memberArrayList.add(new Member("John Doe", "General", "Mar 16", "Cara"));
-        memberArrayList.add(new Member("Sarah Williams", "General"));
-        memberArrayList.add(new Member("Joe Bider", "Team", "Jan 4", "Peter"));
-        memberArrayList.add(new Member("Sean Parker", "General"));
-        */
-
-        db.collection(Constants.FIRESTORE_COLLECTION_MEMBERS).get()
-                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+        db.collection(Constants.Firestore.COLLECTION_MEMBERS)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
-                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots,
+                                        @Nullable FirebaseFirestoreException e) {
 
-                        for (QueryDocumentSnapshot data : queryDocumentSnapshots) {
-                            Member member = data.toObject(Member.class);
-                            Log.d(TAG, "onSuccess: Member Name: ");
+                        memberArrayList.clear();
+
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                            Member member = documentSnapshot.toObject(Member.class);
+                            Log.e(TAG, "onEvent: MEMBER DOCUMENT: " + documentSnapshot.getId());
+                            memberArrayList.add(member);
                         }
+                        memberListAdapter.notifyDataSetChanged();
                     }
                 });
+
+
     }
 
     private void logoutUser() {
@@ -176,6 +211,21 @@ public class HomeFragment extends Fragment implements
         Intent loginActivityIntent = new Intent(getActivity(), LoginActivity.class);
         loginActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(loginActivityIntent);
+    }
+
+    private boolean isPhoneCallPermissionGranted() {
+        return ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CALL_PHONE)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPhoneCallPermission() {
+        String[] permissionToRequest  = { Manifest.permission.CALL_PHONE };
+        ActivityCompat.requestPermissions(getActivity(), permissionToRequest, CALL_PERMISSION_REQUEST_CODE);
+    }
+
+    private void makePhoneCallToMember(String memberContactNumber) {
+        String dial = "tel:" + memberContactNumber;
+        startActivityForResult(new Intent(Intent.ACTION_CALL, Uri.parse(dial)), CALL_INTENT_REQUEST_CODE);
     }
 
     public interface HomeFragmentListener {
